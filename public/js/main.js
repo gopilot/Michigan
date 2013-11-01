9d58fc2617be140716648889d1f7b1814f006b5d
Gabe Boning <gboning@gmail.com>
2013-11-01 11:16:03 -0400

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+(function() {
+  console.log("hello from a require'd coffee file (via assets/js/_helper.coffee)");
+
+}).call(this);
+
+(function() {
+  require.config({
+    paths: {
+      jquery: 'jquery-1.9.1.min'
+    }
+  });
+
+  require(['jquery'], function($) {
+    console.log('jquery loaded (via assets/js/main.coffee)');
+    return $(".borderwrap").height($(document).height());
+  });
+
+}).call(this);
